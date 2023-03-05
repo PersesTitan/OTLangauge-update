3aea78e13a1fe51896532fa3beba4678588bff3e
PersesTitan <torampersesTitan@gmail.com>
2023-03-06 00:10:20 +0900

    Create DefineKMTool.java

---
+++ src/main/java/bin/apply/km/DefineKMTool.java
@@ -0,0 +1,16 @@
+package bin.apply.km;
+
+import bin.exception.MatchException;
+import bin.repository.TypeMap;
+
+public interface DefineKMTool {
+    default void setParam(TypeMap repository, Object[] values,
+                          String[] paramType, String[] paramName) {
+        int len = paramType.length;
+        if (len == values.length) {
+            for (int i = 0; i < len; i++) {
+                repository.create(paramType[i], paramName[i], values[i]);
+            }
+        } else throw MatchException.PARAM_MATCH_ERROR.getThrow(Integer.toString(values.length));
+    }
+}
