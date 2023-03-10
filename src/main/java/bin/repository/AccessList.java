0f2f11084dee91d62adcbb58d00366011fd6aa3c
PersesTitan <torampersesTitan@gmail.com>
2023-03-11 00:01:37 +0900

    Update AccessList.java

--- src/main/java/bin/repository/AccessList.java
+++ src/main/java/bin/repository/AccessList.java
@@ -1,6 +1,7 @@
 package bin.repository;
 
 import bin.exception.VariableException;
+import bin.token.EditToken;
 import bin.token.Token;
 
 import java.util.LinkedList;
@@ -17,49 +18,46 @@ public class AccessList extends LinkedList<TypeMap> {
     }
 
     public void create(String type, String name, Object value) {
-        int accessLevel = this.getAccess(name);
-        if (isLevel(accessLevel)) {
-            super.get(accessLevel).create(type, name, value);
-        } else throw VariableException.ACCESS_ERROR.getThrow(name);
+        super.getFirst().create(type, name, value);
     }
 
     public void update(String name, Object value) {
-        int accessLevel = this.getAccess(name);
+        int accessLevel = EditToken.getAccess(name);
         if (isLevel(accessLevel)) {
             super.get(accessLevel).update(name, value);
         } else throw VariableException.ACCESS_ERROR.getThrow(name);
     }
 
     public void update(String type, String name, Object value) {
-        int accessLevel = this.getAccess(name);
+        int accessLevel = EditToken.getAccess(name);
         if (isLevel(accessLevel)) {
             super.get(accessLevel).update(type, name, value);
         } else throw VariableException.ACCESS_ERROR.getThrow(name);
     }
 
     public Object get(String name) {
-        int accessLevel = getAccess(name);
+        int accessLevel = EditToken.getAccess(name);
         if (isLevel(accessLevel)) {
             return super.get(accessLevel).find(name.substring(accessLevel));
         } else throw VariableException.ACCESS_ERROR.getThrow(name);
     }
 
     public Object get(String type, String name) {
-        int accessLevel = getAccess(name);
+        int accessLevel = EditToken.getAccess(name);
         if (isLevel(accessLevel)) {
             return super.get(accessLevel).find(type, name.substring(accessLevel));
         } else throw VariableException.ACCESS_ERROR.getThrow(name);
     }
 
     public HpMap getMap(String name) {
-        int accessLevel = getAccess(name);
+        int accessLevel = EditToken.getAccess(name);
         if (isLevel(accessLevel)) {
             return super.get(accessLevel).findMap(name.substring(accessLevel));
         } else throw VariableException.ACCESS_ERROR.getThrow(name);
     }
 
     public boolean find(String name) {
-        int accessLevel = this.getAccess(name);
+        int accessLevel = EditToken.getAccess(name);
         if ((name = name.substring(accessLevel)).contains(ACCESS)
                 || name.contains(" ")
                 || name.contains(Token.PUT)
@@ -68,7 +66,7 @@ public class AccessList extends LinkedList<TypeMap> {
     }
 
     public boolean find(String type, String name) {
-        int accessLevel = this.getAccess(name);
+        int accessLevel = EditToken.getAccess(name);
         if ((name = name.substring(accessLevel)).contains(ACCESS)
                 || name.contains(" ")
                 || !isLevel(accessLevel)) return false;
@@ -77,15 +75,5 @@ public class AccessList extends LinkedList<TypeMap> {
 
     private boolean isLevel(int level) {
         return super.size() > level;
-    }
-
-    private int getAccess(String name) {
-        char access = ACCESS.charAt(0);
-        int count = 0;
-        for (int i = 0; i < name.length(); i++) {
-            if (name.charAt(i) == access) count++;
-            else break;
-        }
-        return count;
     }
 }
