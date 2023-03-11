2ed08103797e53482417c0e35b941cbc4cad52f6
PersesTitan <torampersesTitan@gmail.com>
2023-03-12 00:09:52 +0900

    Update MatchException.java

--- src/main/java/bin/exception/MatchException.java
+++ src/main/java/bin/exception/MatchException.java
@@ -1,5 +1,7 @@
 package bin.exception;
 
+import bin.apply.calculator.number.NumberCalculator;
+import bin.token.SeparatorToken;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,14 +10,18 @@ import java.util.concurrent.atomic.AtomicReference;
 @Getter
 @RequiredArgsConstructor
 public enum MatchException implements ExceptionTool {
+    SYSTEM_ERROR("시스템 에러가 발생하였습니다."),
     GRAMMAR_ERROR("문법이 일치하지 않습니다."),
     PARAM_COUNT_ERROR("파라미터 길이가 유효하지 않습니다."),
     ZONE_MATCH_ERROR("괄호가 일치하지 않아 존을 사용할 수 없습니다."),
     DEFINE_CLASS_NAME("이미 정의된 클래스 이름입니다."),
     DEFINE_METHOD_NAME("이미 정의된 메소드 이름입니다."),
     CALCULATOR_ERROR("계산식 중 유효하지 않는 토큰이 존재합니다."),
+    SING_NUMBER_ERROR("숫자 계산에 유효하지 않는 토큰입니다."),
     PARAM_MATCH_ERROR("파라미터 문법이 일치하지 않습니다."),
-    MAP_MATCH_ERROR("맵에서는 =를 사용하여 값을 구분해주어야합니다.")
+    MAP_MATCH_ERROR("맵에서는 =를 사용하여 값을 구분해주어야합니다."),
+    CREATE_KLASS_ERROR("해당 위치에서 클래스를 생성할 수 없습니다."),
+    CREATE_METHOD_ERROR("해당 위치에서 메소드를 생성 할 수 없습니다.")
     ;
 
     private AtomicReference<String> errorCode;
@@ -24,6 +30,12 @@ public enum MatchException implements ExceptionTool {
     @Override
     public String getSubMessage() {
         return switch (this) {
+            case SYSTEM_ERROR ->
+                    """
+                    A system error has occurred. %s
+                    A problem has occurred that should not have occurred.
+                    Please contact the developer.
+                    """;
             case GRAMMAR_ERROR ->
                     """
                     The grammar does not match. %s
@@ -64,6 +76,22 @@ public enum MatchException implements ExceptionTool {
                     In maps, you must use = to separate values. %s
                     Please check the grammar again.
                     """;
+            case CREATE_KLASS_ERROR ->
+                    """
+                    Cannot create a class from that location. %s
+                    Please check the grammar again.
+                    """;
+            case CREATE_METHOD_ERROR ->
+                    """
+                    Cannot create a method from that location. %s
+                    Please check the grammar again.
+                    """;
+            case SING_NUMBER_ERROR ->
+                    "Please check the grammar again.Invalid token for numeric calculation. %s" +
+                    SeparatorToken.SEPARATOR_LINE +
+                    "Token : (" + String.join(", ", NumberCalculator.getInstance().numbers.keySet()) + ")" +
+                    SeparatorToken.SEPARATOR_LINE +
+                    "Please check the grammar again.";
         };
     }
 
