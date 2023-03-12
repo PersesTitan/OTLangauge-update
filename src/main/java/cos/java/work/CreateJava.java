ae4ad01691f30315c494c73bf07719a1b667e82f
PersesTitan <torampersesTitan@gmail.com>
2023-03-13 00:43:01 +0900

    Create CreateJava.java

---
+++ src/main/java/cos/java/work/CreateJava.java
@@ -0,0 +1,17 @@
+package cos.java.work;
+
+import work.CreateWork;
+
+public class CreateJava extends CreateWork<Object> {
+    public CreateJava(Class<Object> klass, String klassName, String... params) {
+        super(klass, klassName, params);
+    }
+
+    @Override
+    protected Object createItem(Object[] params) {
+        return null;
+    }
+
+    @Override
+    public void reset() {}
+}
