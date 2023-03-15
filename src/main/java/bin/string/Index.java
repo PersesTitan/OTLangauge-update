3ab8d6dfeb21e8f6efe0d8980ca0d00b23db5ebd
PersesTitan <torampersesTitan@gmail.com>
2023-03-16 00:24:15 +0900

    Create Index.java

---
+++ src/main/java/bin/string/Index.java
@@ -0,0 +1,19 @@
+package bin.string;
+
+import bin.token.KlassToken;
+import bin.variable.Types;
+import work.ReplaceWork;
+
+public class Index extends ReplaceWork {
+    public Index() {
+        super(KlassToken.STRING_VARIABLE, false, KlassToken.STRING_VARIABLE);
+    }
+
+    @Override
+    protected Object replaceItem(Object klassValue, Object[] params) {
+        return klassValue.toString().indexOf(params[0].toString());
+    }
+
+    @Override
+    public void reset() {}
+}
