c09be4390be2fd6aa150caed51d93c606072f4e8
PersesTitan <torampersesTitan@gmail.com>
2023-03-16 00:24:08 +0900

    Update WorkMap.java

--- src/main/java/bin/repository/WorkMap.java
+++ src/main/java/bin/repository/WorkMap.java
@@ -1,19 +1,23 @@
 package bin.repository;
 
 import bin.exception.VariableException;
+import bin.token.CheckToken;
+import work.WorkTool;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class WorkMap<W> extends HashMap<String, Map<String, W>> {
+public class WorkMap<W extends WorkTool> extends HashMap<String, Map<String, W>> {
     public void put(String klass, String method, W work) {
+        if (work.isStatic() && CheckToken.isKlass(method))
+            throw VariableException.STATIC_METHOD_NAME_ERROR.getThrow(method);
         if (super.containsKey(klass)) super.get(klass).put(method, work);
         else super.put(klass, new HashMap<>(Map.of(method, work)));
     }
 
     public W get(String klass, String method) {
         Map<String, W> map = super.get(klass);
-        if (map.containsKey(method)) return super.get(klass).get(method);
+        if (map.containsKey(method)) return map.get(method);
         else throw VariableException.NO_DEFINE_NAME.getThrow(method);
     }
 
