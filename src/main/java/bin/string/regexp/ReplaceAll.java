3d3a9d33fe23f8d6c99317085d4018ffcd5909cf
PersesTitan <torampersesTitan@gmail.com>
2023-03-16 00:24:20 +0900

    Create ReplaceAll.java

---
+++ src/main/java/bin/string/regexp/ReplaceAll.java
@@ -0,0 +1,18 @@
+package bin.string.regexp;
+
+import bin.token.KlassToken;
+import work.ReplaceWork;
+
+public class ReplaceAll extends ReplaceWork {
+    public ReplaceAll() {
+        super(KlassToken.STRING_VARIABLE, false, KlassToken.STRING_VARIABLE, KlassToken.STRING_VARIABLE);
+    }
+
+    @Override
+    protected Object replaceItem(Object klassValue, Object[] params) {
+        return klassValue.toString().replaceAll(params[0].toString(), params[1].toString());
+    }
+
+    @Override
+    public void reset() {}
+}
