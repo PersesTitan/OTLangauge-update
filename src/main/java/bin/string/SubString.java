32776787076477457c263be81dd551abdb139f71
PersesTitan <torampersesTitan@gmail.com>
2023-03-17 00:25:14 +0900

    Create SubString.java

---
+++ src/main/java/bin/string/SubString.java
@@ -0,0 +1,19 @@
+package bin.string;
+
+import bin.token.KlassToken;
+import bin.variable.Types;
+import work.ReplaceWork;
+
+public class SubString extends ReplaceWork {
+    public SubString() {
+        super(KlassToken.STRING_VARIABLE, false, KlassToken.INT_VARIABLE, KlassToken.INT_VARIABLE);
+    }
+
+    @Override
+    protected Object replaceItem(Object klassValue, Object[] params) {
+        return Types.toString(klassValue).substring((int) params[0], (int) params[1]);
+    }
+
+    @Override
+    public void reset() {}
+}
