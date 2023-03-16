847d3e8956289b3999531957ff10a34ee0a3f764
PersesTitan <torampersesTitan@gmail.com>
2023-03-17 00:25:19 +0900

    Create ToUpperCase.java

---
+++ src/main/java/bin/string/tocase/ToUpperCase.java
@@ -0,0 +1,20 @@
+package bin.string.tocase;
+
+import bin.token.KlassToken;
+import work.ReplaceWork;
+
+import java.util.Locale;
+
+public class ToUpperCase extends ReplaceWork {
+    public ToUpperCase() {
+        super(KlassToken.STRING_VARIABLE, false);
+    }
+
+    @Override
+    protected Object replaceItem(Object klassValue, Object[] params) {
+        return klassValue.toString().toUpperCase(Locale.ROOT);
+    }
+
+    @Override
+    public void reset() {}
+}
