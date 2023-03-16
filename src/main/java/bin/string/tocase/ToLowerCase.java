ac785dc5b7ea8ad9bba4a679987f6b0f2eb45c23
PersesTitan <torampersesTitan@gmail.com>
2023-03-17 00:25:17 +0900

    Create ToLowerCase.java

---
+++ src/main/java/bin/string/tocase/ToLowerCase.java
@@ -0,0 +1,20 @@
+package bin.string.tocase;
+
+import bin.token.KlassToken;
+import work.ReplaceWork;
+
+import java.util.Locale;
+
+public class ToLowerCase extends ReplaceWork {
+    public ToLowerCase() {
+        super(KlassToken.STRING_VARIABLE, false);
+    }
+
+    @Override
+    protected Object replaceItem(Object klassValue, Object[] params) {
+        return klassValue.toString().toLowerCase(Locale.ROOT);
+    }
+
+    @Override
+    public void reset() {}
+}
