c4602b00c7c0bc57b68f0fa1c49585c2fb90d5d8
PersesTitan <torampersesTitan@gmail.com>
2023-03-23 00:12:11 +0900

    Update Read.java

--- src/main/java/bin/apply/Read.java
+++ src/main/java/bin/apply/Read.java
@@ -1,9 +1,11 @@
 package bin.apply;
 
 import bin.Repository;
+import bin.apply.run.Import;
 import bin.repository.code.CodeMap;
 import bin.system.loop.For;
 import bin.token.CheckToken;
+import bin.token.KlassToken;
 
 import java.util.Map;
 
@@ -30,6 +32,10 @@ public class Read {
     public static int startLine(String line, String path, int i, String repoKlass) {
         if (line.isEmpty()) return i + 1;
 
+        if (line.startsWith(KlassToken.IMPORT)) {
+            new Import().start(line);
+            return i + 1;
+        }
         if (CheckToken.startWith(line, '!')) line = line.substring(1);
         else if (Replace.checkToken(line)) line = Replace.replaceToken(line);
         if (For.check(line)) return For.start(line, path, i, repoKlass) + 1;
