b35d2ee1b0b66770d4aee4c269d71f0d09c43dc7
PersesTitan <torampersesTitan@gmail.com>
2023-03-25 10:46:16 +0900

    Create StringToken.java

---
+++ src/main/java/bin/token/StringToken.java
@@ -0,0 +1,17 @@
+package bin.token;
+
+import java.util.Locale;
+
+public interface StringToken {
+    String REPLACE = "ㄹㅂㄹ";
+    String REPLACE_ALL = "ㄹㅍㄹ";
+    String SPLIT = "ㅅㅂㅅ";
+    String SPLIT_ALL = "ㅅㅍㅅ";
+
+    String CONTAINS = "ㅋㅇㅋ";
+    String INDEX = "ㅇㄴㅇ";
+    String SUBSTRING = "ㅅㅆㅅ";
+
+    String TO_UPPER_CASE = "ㅇㅍㅇ";
+    String TO_LOWER_CASE = "ㄷㅍㄷ";
+}
