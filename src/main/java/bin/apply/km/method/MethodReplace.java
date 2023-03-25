895a81ae6327e4d2c491ee9472a15053269bbc82
PersesTitan <torampersesTitan@gmail.com>
2023-03-25 10:46:09 +0900

    Update MethodReplace.java

--- src/main/java/bin/apply/km/method/MethodReplace.java
+++ src/main/java/bin/apply/km/method/MethodReplace.java
@@ -1,8 +1,9 @@
 package bin.apply.km.method;
 
 import bin.Repository;
-import bin.apply.Replace;
+import bin.apply.ReplaceType;
 import bin.apply.km.klass.KlassItem;
+import bin.apply.mode.LoopMode;
 import bin.exception.MatchException;
 import bin.repository.TypeMap;
 import work.ReplaceWork;
@@ -10,8 +11,8 @@ import work.ReplaceWork;
 public class MethodReplace extends ReplaceWork {
     private final DefineMethod method;
 
-    public MethodReplace(DefineMethod method) {
-        super(method.getKlassName(), false, method.getParamType());
+    public MethodReplace(DefineMethod method, boolean isStatic) {
+        super(method.getKlassName(), method.getReturnType(), isStatic, method.getParamType());
         if (method.getReturnVarName() == null) {
             String errorMessage = method.getCode().get(method.getEnd());
             throw MatchException.SYSTEM_ERROR.getThrow(errorMessage);
@@ -22,35 +23,46 @@ public class MethodReplace extends ReplaceWork {
     @Override
     protected Object replaceItem(Object klassValue, Object[] params) {
         KlassItem klassItem = (KlassItem) klassValue;
-        TypeMap klassRepository = klassItem.getRepository(),
-                methodRepository = new TypeMap(),
-                deleteRepository;
+        TypeMap methodRepository = new TypeMap();
+        TypeMap deleteRepository;
         Object value;
-        try {
-            // 1. 클래스 저장소 추가
-            // 2. 메소드의 임시 저장소 추가
-            Repository.repositoryArray.addFirst(klassRepository);
-            Repository.repositoryArray.addFirst(methodRepository);
-            // 초기 파라미터 값 넣기
-            this.method.setParam(methodRepository, params);
-            this.method.start();
-            value = Replace.replace(this.method.getReturnVarName());
-        } finally {
-            // 1. 메소드 임시 저장소 제거
-            // 2. 클래스 임시 저장소 제거 및 제거 된 저장소 일치 확인
-            Repository.repositoryArray.removeFirst();
-            deleteRepository = Repository.repositoryArray.removeFirst();
+        if (klassItem == null) {
+            // system method (static)
+            try {
+                Repository.repositoryArray.addFirst(methodRepository);
+                this.method.setParam(methodRepository, params);
+                this.method.start();
+                value = ReplaceType.replace(
+                        this.method.getReturnType(),
+                        this.method.getReturnVarName()
+                );
+            } finally {
+                deleteRepository = Repository.repositoryArray.removeFirst();
+            }
+        } else {
+            try {
+                // 1. 클래스 저장소 추가
+                // 2. 메소드의 임시 저장소 추가
+                TypeMap klassRepository = klassItem.getRepository();
+                Repository.repositoryArray.addFirst(klassRepository);
+                Repository.repositoryArray.addFirst(methodRepository);
+                // 초기 파라미터 값 넣기
+                this.method.setParam(methodRepository, params);
+                this.method.start();
+                value = ReplaceType.replace(
+                        this.method.getReturnType(),
+                        this.method.getReturnVarName()
+                );
+            } finally {
+                // 1. 메소드 임시 저장소 제거
+                // 2. 클래스 임시 저장소 제거 및 제거 된 저장소 일치 확인
+                Repository.repositoryArray.removeFirst();
+                deleteRepository = Repository.repositoryArray.removeFirst();
+            }
         }
 
-        if (klassRepository != deleteRepository) {
-            String errorMessage = klassRepository.toString()
-                    .concat(" != ")
-                    .concat(deleteRepository.toString());
-            throw MatchException.SYSTEM_ERROR.getThrow(errorMessage);
-        }
+        if (methodRepository != deleteRepository)
+            throw MatchException.SYSTEM_ERROR.getThrow(methodRepository + " != " + deleteRepository);
         return value;
     }
-
-    @Override
-    public void reset() {}
 }
