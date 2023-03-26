7c7c462547d9672551e5a272efe8a3a4fb164dec
PersesTitan <torampersesTitan@gmail.com>
2023-03-27 00:06:48 +0900

    Create PrintTab.java

---
+++ src/main/java/bin/system/console/out/PrintTab.java
@@ -0,0 +1,19 @@
+package bin.system.console.out;
+
+import bin.token.KlassToken;
+import bin.variable.Types;
+import work.StartWork;
+
+public class PrintTab extends StartWork {
+    public PrintTab() {
+        super(KlassToken.SYSTEM, true, KlassToken.STRING_VARIABLE);
+    }
+
+    @Override
+    protected void startItem(Object klassValue, Object[] params) {
+        System.out.print(Types.toString(params[0]).concat("\t"));
+    }
+
+    @Override
+    public void reset() {}
+}
