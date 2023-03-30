8903d33bbaee95ca157e3d06a54bd0711a611198
PersesTitan <torampersesTitan@gmail.com>
2023-03-31 01:55:39 +0900

    Update DefineKMTool.java

--- src/main/java/bin/apply/km/DefineKMTool.java
+++ src/main/java/bin/apply/km/DefineKMTool.java
@@ -8,9 +8,7 @@ public interface DefineKMTool {
                           String[] paramType, String[] paramName) {
         int len = paramType.length;
         if (len == values.length) {
-            for (int i = 0; i < len; i++) {
-                repository.create(paramType[i], paramName[i], values[i]);
-            }
+            for (int i = 0; i < len; i++) repository.create(paramType[i], paramName[i], values[i]);
         } else throw MatchException.PARAM_MATCH_ERROR.getThrow(Integer.toString(values.length));
     }
 }
