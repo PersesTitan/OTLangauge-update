441f98a9d0d1ac5c9afe035c36111b4e76d86f9e
PersesTitan <torampersesTitan@gmail.com>
2023-04-01 00:48:42 +0900

    Update MethodVoid.java

--- src/main/java/bin/apply/km/method/MethodVoid.java
+++ src/main/java/bin/apply/km/method/MethodVoid.java
@@ -3,32 +3,23 @@ package bin.apply.km.method;
 import bin.apply.km.klass.KlassItem;
 import bin.apply.loop.Loop;
 import bin.exception.MatchException;
-import bin.repository.TypeMap;
+import bin.exception.SystemException;
 import work.StartWork;
 
 public class MethodVoid extends StartWork {
     private final DefineMethod method;
 
-    public MethodVoid(DefineMethod method) {
-        super(method.getKlassName(), false, method.getParamType());
+    public MethodVoid(DefineMethod method, boolean isStatic) {
+        super(method.getKlassName(), isStatic, method.getParamType());
         if (method.getReturnVarName() != null) {
             String errorMessage = method.getCode().get(method.getEnd());
-            throw MatchException.SYSTEM_ERROR.getThrow(errorMessage);
+            throw SystemException.SYSTEM_ERROR.getThrow(errorMessage);
         }
         this.method = method;
     }
 
     @Override
     protected void startItem(Object klassValue, Object[] params) {
-        KlassItem klassItem = (KlassItem) klassValue;
-        TypeMap methodRepository = new TypeMap();
-        Loop.SET_KM(klassItem.getRepository(), methodRepository, () -> {
-            // 초기 파라미터 값 넣기
-            this.method.setParam(methodRepository, params);
-            this.method.start();
-        });
+        Loop.startMethod(this.method, (KlassItem) klassValue, params);
     }
-
-    @Override
-    public void reset() {}
 }
