f9209da735e6f5fd0de5091aa48084a6e830fb0e
PersesTitan <torampersesTitan@gmail.com>
2023-04-05 00:47:53 +0900

    Update MatchException.java

--- src/main/java/bin/exception/MatchException.java
+++ src/main/java/bin/exception/MatchException.java
@@ -98,6 +98,7 @@ public enum MatchException implements ExceptionTool {
     @Override
     public Error getThrow(String errorCode) {
         if (this.errorCode == null) this.errorCode = new AtomicReference<>(errorCode);
+        else this.errorCode.set(errorCode);
         return new Error(this.name(), this.getClass());
     }
 }
