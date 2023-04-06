6e1e20db84764a0b1a6f39a48e13f1a08386fe3d
PersesTitan <torampersesTitan@gmail.com>
2023-04-06 10:08:33 +0900

    Update Reset.java

--- src/main/java/cos/file/Reset.java
+++ src/main/java/cos/file/Reset.java
@@ -1,11 +1,25 @@
 package cos.file;
 
 import bin.Repository;
+import bin.token.KlassToken;
+import cos.file.etc.CreateFile;
+import cos.file.etc.FileToken;
+import cos.file.item.FileItem;
+import cos.file.work.FileIsDirectory;
+import cos.file.work.FileIsFile;
+import cos.file.work.FileList;
+import cos.file.work.FileRead;
+import work.ReplaceWork;
 import work.ResetWork;
 
-public class Reset implements ResetWork {
+public class Reset implements ResetWork, KlassToken, FileToken {
     @Override
     public void reset() {
+        Repository.createWorks.put(FILE, new CreateFile());
 
+        Repository.replaceWorks.put(LIST_STRING, FILE, FILE_LIST, new FileList());
+        Repository.replaceWorks.put(LIST_STRING, FILE, FILE_READ, new FileRead());
+        Repository.replaceWorks.put(BOOL_VARIABLE, FILE, IS_FILE, new FileIsFile());
+        Repository.replaceWorks.put(BOOL_VARIABLE, FILE, IS_DIRECTORY, new FileIsDirectory());
     }
 }
