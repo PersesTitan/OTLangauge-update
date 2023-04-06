6c9258125e2fbe0f0b7c6de5eda4c6da9f9b4c55
PersesTitan <torampersesTitan@gmail.com>
2023-04-07 00:07:49 +0900

    Create GuiToken.java

---
+++ src/main/java/cos/gui/etc/GuiToken.java
@@ -0,0 +1,61 @@
+package cos.gui.etc;
+
+import bin.variable.Types;
+
+import javax.swing.*;
+
+public interface GuiToken {
+    String GUI = "ㄱㅁㄱ";
+    String GROUP = GUI + "-ㄱㄹㄱ";
+    String EVENT = GUI + "-ㅇㅃㅇ";
+
+    String FRAME = "ㅍㄹㅍ";
+    String PANEL = "ㅍㅇㅍ";
+
+    String BUTTON           = "ㅂㅇㅂ";
+    String CHECK_BOX        = "ㅊㅋㅊ";
+    String RADIO_BUTTON     = "ㄹㅂㄹ";
+    String TEXT_FIELD       = "ㅌㅍㅌ";
+    String PASSWORD_FILED   = "ㅍㅍㅍ";
+    String TEXT_AREA        = "ㅌㅇㅌ";
+
+    String COMBO_BOX_INT       = GUI + "ㅋㅈㅋ";
+    String COMBO_BOX_LONG      = GUI + "ㅋㅉㅋ";
+    String COMBO_BOX_BOOL      = GUI + "ㅋㅂㅋ";
+    String COMBO_BOX_STRING    = GUI + "ㅋㅁㅋ";
+    String COMBO_BOX_CHARACTER = GUI + "ㅋㄱㅋ";
+    String COMBO_BOX_FLOAT     = GUI + "ㅋㅅㅋ";
+    String COMBO_BOX_DOUBLE    = GUI + "ㅋㅆㅋ";
+
+    // METHOD
+    String ADD = "<<";
+    String ADD_EVENT = "<ㅇㅃㅇ";
+    String SET_MODEL = "<ㅁㄷㅁ";
+    String ADD_KEY = "<ㅋㅂㅋ";
+    String ADD_MOUSE = "<ㅁㅇㅁ";
+
+    String SET_VISIBLE = "<ㅂㅈㅂ";
+    String GET_VISIBLE = ">ㅂㅈㅂ";
+    String SET_ENABLE = "<ㅎㅅㅎ";
+    String GET_ENABLE = ">ㅎㅅㅎ";
+
+    String GET_LOCATION = ">ㄹㅋㄹ";
+
+    String SET_SIZE = "<ㅆㅈㅆ";
+    String GET_SIZE = ">ㅆㅈㅆ";
+    String GET_WIDTH = ">ㄴㅂㄴ";  // 너비
+    String GET_HEIGHT = ">ㄴㅍㄴ"; // 높이
+    String SET_WIDTH = "<ㄴㅂㄴ";  // 너비
+    String SET_HEIGHT = "<ㄴㅍㄴ"; // 높이
+
+    String GET_X = ">ㄱㄹㄱ";      // 가로
+    String GET_Y = ">ㅅㄹㅅ";      // 세로
+    String SET_X = "<ㄱㄹㄱ";      // 가로
+    String SET_Y = "<ㅅㄹㅅ";      // 세로
+
+    String SET_TEXT = "<ㅌㅅㅌ";
+    String GET_TEXT = ">ㅌㅅㅌ";
+    String SET_NAME = "<ㅇㄹㅇ";
+    String GET_NAME = ">ㅇㄹㅇ";
+
+}
