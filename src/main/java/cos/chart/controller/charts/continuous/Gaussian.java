c257da5ec38f1408c7b22a150a3e34a32beb1f66
PersesTitan <torampersesTitan@gmail.com>
2023-04-08 00:10:11 +0900

    Create Gaussian.java

---
+++ src/main/java/cos/chart/controller/charts/continuous/Gaussian.java
@@ -0,0 +1,23 @@
+package cos.chart.controller.charts.continuous;
+
+import cos.chart.controller.plotting.ContinuousFunctionPlotter;
+
+public class Gaussian extends ContinuousFunctionPlotter {
+    protected double stdDeviation, variance, mean;
+
+    public Gaussian(double stdDeviation, double mean) {
+        this.stdDeviation = stdDeviation;
+        variance = stdDeviation * stdDeviation;
+        this.mean = mean;
+    }
+
+    @Override
+    public double getY(double x) {
+        return Math.pow(Math.exp(-(((x - mean) * (x - mean)) / ((2 * variance)))), 1 / (stdDeviation * Math.sqrt(2 * Math.PI)));
+    }
+
+    @Override
+    public String getName() {
+        return "Gaussian Curve";
+    }
+}
