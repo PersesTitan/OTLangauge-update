8194cbb2bd06f9e74af7a79df55386f761162347
PersesTitan <torampersesTitan@gmail.com>
2023-04-15 00:23:20 +0900

    Update CreateBoolean.java

--- src/main/java/bin/variable/origin/CreateBoolean.java
+++ src/main/java/bin/variable/origin/CreateBoolean.java
@@ -1,5 +1,6 @@
 package bin.variable.origin;
 
+import bin.apply.calculator.bool.BoolCalculator;
 import bin.exception.VariableException;
 import bin.token.KlassToken;
 import bin.token.Token;
@@ -14,7 +15,15 @@ public class CreateBoolean extends CreateWork<Boolean> {
     protected Object createItem(Object... params) {
         String value = params[0].toString();
         if (value.equals(Token.TRUE) || value.equals(Token.FALSE)) return value.equals(Token.TRUE);
+        else if (this.haveBoolCalculator(value)) return BoolCalculator.getInstance().calculator(value);
         else throw VariableException.VALUE_ERROR.getThrow(value);
+    }
+
+    private boolean haveBoolCalculator(String value) {
+        return value.indexOf(Token.OR_C) >= 0
+                || value.indexOf(Token.AND_C) >= 0
+                || value.contains(Token.NOT)
+                || Token.compares.stream().anyMatch(value::contains);
     }
 
     @Override
