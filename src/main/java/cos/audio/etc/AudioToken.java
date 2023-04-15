843a6f0a71f90c3ac28e7acdc7aebfdd1b4cd7cb
PersesTitan <torampersesTitan@gmail.com>
2023-04-16 00:55:16 +0900

    Create AudioToken.java

---
+++ src/main/java/cos/audio/etc/AudioToken.java
@@ -0,0 +1,17 @@
+package cos.audio.etc;
+
+public interface AudioToken {
+    String AUDIO = "ㅇㄷㅇ";
+
+    String SET_SPEED = "<ㅅㄱㅅ";
+    String SET_LOOP = "<ㄹㅍㄹ";
+    String SET_VOLUME = "<ㅇㄹㅇ";
+    String SET_PITCH = "<ㅍㅊㅍ";
+
+    String GET_SPEED = ">ㅅㄱㅅ";
+    String GET_LOOP = ">ㄹㅍㄹ";
+    String GET_VOLUME = ">ㅇㄹㅇ";
+    String GET_PITCH = ">ㅍㅊㅍ";
+
+    String START = "ㅅㅌㅅ";
+}
