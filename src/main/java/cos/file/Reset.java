76a1ed42b2c2745d5228914effd1011dfc30e177
PersesTitan <torampersesTitan@gmail.com>
2023-04-20 00:15:58 +0900

    Update Reset.java

--- src/main/java/cos/file/Reset.java
+++ src/main/java/cos/file/Reset.java
@@ -5,11 +5,6 @@ import bin.token.KlassToken;
 import cos.file.etc.CreateFile;
 import cos.file.etc.FileToken;
 import cos.file.item.FileItem;
-import cos.file.work.FileIsDirectory;
-import cos.file.work.FileIsFile;
-import cos.file.work.FileList;
-import cos.file.work.FileRead;
-import work.ReplaceWork;
 import work.ResetWork;
 
 public class Reset implements ResetWork, KlassToken, FileToken {
@@ -17,9 +12,11 @@ public class Reset implements ResetWork, KlassToken, FileToken {
     public void reset() {
         Repository.createWorks.put(FILE, new CreateFile());
 
-        Repository.replaceWorks.put(LIST_STRING, FILE, FILE_LIST, new FileList());
-        Repository.replaceWorks.put(LIST_STRING, FILE, FILE_READ, new FileRead());
-        Repository.replaceWorks.put(BOOL_VARIABLE, FILE, IS_FILE, new FileIsFile());
-        Repository.replaceWorks.put(BOOL_VARIABLE, FILE, IS_DIRECTORY, new FileIsDirectory());
+        AddWork<FileItem> addWork = new AddWork<>(FILE);
+
+        addWork.addR(LIST_STRING, FILE_LIST, FileItem::fileList);
+        addWork.addR(LIST_STRING, FILE_READ, FileItem::readFile);
+        addWork.addR(b, IS_FILE, FileItem::isFile);
+        addWork.addR(b, IS_DIRECTORY, FileItem::isDirectory);
     }
 }
