69904269cb9c7c679b2d642c0b7eb366fbdd7fd1
PersesTitan <torampersesTitan@gmail.com>
2023-04-22 12:28:03 +0900

    Create GraphToken.java

---
+++ src/main/java/cos/graph/etc/GraphToken.java
@@ -0,0 +1,16 @@
+package cos.graph.etc;
+
+import cos.gui.etc.GuiToken;
+
+public interface GraphToken {
+    String GRAPH = "ㄱㄹㄱ";
+
+    String CREATE_ADD = "<<ㅅㅇㅅ";
+    String ADD = "<<";
+
+    String GUI = GuiToken.GUI;
+    String SET_TEXT = GuiToken.SET_TEXT;
+    String SET_X = GuiToken.SET_X;
+    String SET_Y = GuiToken.SET_Y;
+    String SET_SIZE = GuiToken.SET_SIZE;
+}
