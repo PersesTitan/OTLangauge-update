8a2a927f497003a829f72794f940be8ff4a95830
PersesTitan <torampersesTitan@gmail.com>
2023-04-22 12:28:00 +0900

    Create SettingsUpdateListener.java

---
+++ src/main/java/cos/graph/controller/swing/SettingsUpdateListener.java
@@ -0,0 +1,7 @@
+package cos.graph.controller.swing;
+
+import cos.graph.controller.plotting.PlotSettings;
+
+public interface SettingsUpdateListener {
+    void graphUpdated(PlotSettings settings);
+}
