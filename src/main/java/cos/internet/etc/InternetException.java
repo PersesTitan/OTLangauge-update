b1b91ac9fa7e3ecdf34cd361b97c261bb8e8cfbf
PersesTitan <torampersesTitan@gmail.com>
2023-04-27 01:51:00 +0900

    Create InternetException.java

---
+++ src/main/java/cos/internet/etc/InternetException.java
@@ -0,0 +1,36 @@
+package cos.internet.etc;
+
+import bin.exception.Error;
+import bin.exception.ExceptionTool;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@Getter
+@RequiredArgsConstructor
+public enum InternetException implements ExceptionTool {
+    NOT_VALID_ERROR("유효하지 않는 URL입니다.")
+    ;
+
+    private AtomicReference<String> errorCode;
+    private final String message;
+
+    @Override
+    public String getSubMessage() {
+        return switch (this) {
+            case NOT_VALID_ERROR ->
+                    """
+                    Invalid URL. %s
+                    Please check url.
+                    """;
+        };
+    }
+
+    @Override
+    public Error getThrow(String errorCode) {
+        if (this.errorCode == null) this.errorCode = new AtomicReference<>(errorCode);
+        else this.errorCode.set(errorCode);
+        return new Error(this.name(), this.getClass());
+    }
+}
