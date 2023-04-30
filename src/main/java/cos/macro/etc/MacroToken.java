744523b7b3c97b07be4a7afe9ae7d0edfe0902e5
PersesTitan <torampersesTitan@gmail.com>
2023-05-01 00:33:12 +0900

    Create MacroToken.java

---
+++ src/main/java/cos/macro/etc/MacroToken.java
@@ -0,0 +1,14 @@
+package cos.macro.etc;
+
+public interface MacroToken {
+    String MACRO = "ㅁㅋㅁ";
+
+    String MOUSE1 = "ㅁㅂㅁ1";
+    String MOUSE2 = "ㅁㅂㅁ2";
+    String MOUSE3 = "ㅁㅂㅁ3";
+    String WHEEL = "ㅎㄹㅎ";
+
+    String GET_POINT = ">ㅈㅍㅈ";
+    String GET_X = ">ㄱㄹㄱ";      // 가로
+    String GET_Y = ">ㅅㄹㅅ";      // 세로
+}
