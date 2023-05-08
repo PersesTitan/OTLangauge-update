770a0c07ed89a2a382f4f4b71433be80e7815cf0
PersesTitan <torampersesTitan@gmail.com>
2023-05-08 16:26:53 +0900

    Create OCRToken.java

---
+++ src/main/java/cos/ocr/etc/OCRToken.java
@@ -0,0 +1,12 @@
+package cos.ocr.etc;
+
+public interface OCRToken {
+    String OCR = "ㅇㅋㅇ";
+
+    String SET_LANGUAGE = "<ㅇㄴㅇ";
+    String REMOVE_LANGUAGE = "-ㅇㄴㅇ";
+    String GET_LANGUAGE = ">ㅇㄴㅇ";
+    String INIT_LANGUAGE = "!ㅇㄴㅇ";
+
+    String START = "ㅅㅌㅅ";
+}
