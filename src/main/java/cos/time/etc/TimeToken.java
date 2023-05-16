422726fcb4094dc7377f7b1172a6fdd03bdedc75
PersesTitan <torampersesTitan@gmail.com>
2023-05-17 07:18:05 +0900

    Create TimeToken.java

---
+++ src/main/java/cos/time/etc/TimeToken.java
@@ -0,0 +1,21 @@
+package cos.time.etc;
+
+public interface TimeToken {
+    String TIME = "ㅅㄱㅅ";
+    String DATE = "ㅆㄱㅆ";
+    String DATE_TIME = "ㅅㄱㅆ";
+
+    String NOW = "ㅈㄱㅈ";
+
+    String YEAR     = "ㄴㄴㄴ";    // 년
+    String MONTH    = "ㅇㅂㅇ";    // 월
+    String WEEK     = "ㅈㅅㅈ";    // 주
+    String DAY      = "ㅇㄹㅇ";    // 일
+    String HOUR     = "ㅅㄱㅅ";    // 시
+    String MINUTE   = "ㅂㄴㅂ";    // 분
+    String SECOND   = "ㅊㅅㅊ";    // 초
+    String NANO     = "ㅁㄹㅁ";    // 밀리초
+
+    String IS_BEFORE = "";
+    String IS_AFTER = "";
+}
