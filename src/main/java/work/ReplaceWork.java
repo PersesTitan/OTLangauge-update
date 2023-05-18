2a50ba3cc252fb1c2fe163200681634464731d2d
PersesTitan <torampersesTitan@gmail.com>
2023-05-18 13:40:47 +0900

    Update ReplaceWork.java

--- src/main/java/work/ReplaceWork.java
+++ src/main/java/work/ReplaceWork.java
@@ -1,50 +1,50 @@
 package work;
 
 import bin.Repository;
-import bin.exception.MatchException;
 import bin.exception.VariableException;
-import bin.token.EditToken;
-import bin.variable.Types;
+import bin.token.CheckToken;
+import lombok.Getter;
 
+@Getter
 public abstract class ReplaceWork implements WorkTool {
+    private final boolean isStatic;
+    private final CreateWork<?> returnType;
     private final CreateWork<?> createWork;
     private final String[] params;
     private final int paramLen;
 
-    public ReplaceWork(String klassType, String... params) {
+    public ReplaceWork(String klassType, String returnType, boolean isStatic, String... params) {
         this.params = params;
+        this.isStatic = isStatic;
         this.paramLen = params.length;
-        if (!Repository.isKlass(klassType)) throw VariableException.NO_DEFINE_TYPE.getThrow(klassType);
+        if (!CheckToken.isKlass(returnType)) throw VariableException.NO_DEFINE_TYPE.getThrow(returnType);
+        if (!CheckToken.isKlass(klassType)) throw VariableException.NO_DEFINE_TYPE.getThrow(klassType);
+        this.returnType = Repository.createWorks.get(returnType);
         this.createWork = Repository.createWorks.get(klassType);
-        Repository.checkParamType(params);
+        CheckToken.checkParamType(params);
     }
 
     protected abstract Object replaceItem(Object klassValue, Object[] params);
 
     public Object replace(Object klassValue, String params) {
-        this.checkType(this.createWork, klassValue);
-        switch (this.paramLen) {
-            case 0:
-                if (params != null) throw MatchException.PARAM_COUNT_ERROR.getThrow(params);
-                if (klassValue != null) throw MatchException.GRAMMAR_ERROR.getThrow(klassValue.toString());
-                return this.replaceItem(null, null);
-            case 1:
-                if (params == null) {
-                    if (Types.STRING.originCheck(this.params[0])) {
-                        return this.replaceItem(klassValue, new Object[] {""});
-                    } else {
-                        String errorMessage = createWork.getKlassName()
-                                .concat(" ")
-                                .concat(String.join(", ", this.params));
-                        throw MatchException.PARAM_COUNT_ERROR.getThrow(errorMessage);
-                    }
-                }
-            default:
-                String[] param = EditToken.cutParams(this.paramLen, params);
-                Object[] values = new Object[this.paramLen];
-                for (int i = 0; i < this.paramLen; i++)
-                    values[i] = Repository.createWorks.get(this.params[i]).create(param[i]);
-                return this.replaceItem(klassValue, values);
-        }
+        Object startValues = this.getStartValues(this.createWork, this.isStatic, klassValue);
+        Object[] startParams = this.getStartParams(this.createWork, this.paramLen, params, this.params);
+
+        Object value = this.replaceItem(startValues, startParams);
+        if (this.returnType.check(value)) return value;
+        else throw VariableException.VALUE_ERROR.getThrow("return type error : " + this.returnType.getKlass());
+    }
+
+    @Override
+    public void reset() {}
+
+    @Override
+    public int getSize() {
+        return this.paramLen;
+    }
+
+    @Override
+    public String toString() {
+        return getString(this.createWork, this.paramLen, this.params);
     }
 }
